c5200cee9ee6fa246207df8780775bff10051173
Yuri <wall.mart.boss@proton.me>
2024-03-25 15:38:15 +0200

    Corrections 25/03

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// const text = document.querySelector('#moble-menu');
+// const id = document.ElementById('moble-menu');
+
+// function openModal(text) {
+//   let modal = document.querySelector(text);
+//   modal.classList.add('is-open');
+// }
+// function closeModal(id) {
+//   let modal = document.getElementById(id);
+//   modal.classList.remove('is-open');
+// }
